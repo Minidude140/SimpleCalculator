4782d246197d0b009048a977806683b5b4a468f4
Zac <zac.minidude140@Gmail.com>
2023-09-02 22:22:48 -0600

    Added First number input. **Know Bugs** Will Crash with invalid conversion

--- SimpleCalculator/SayMyName.vb
+++ SimpleCalculator/SayMyName.vb
@@ -12,16 +12,25 @@ Module SayMyName
 
     Sub Main()
         'TODO
-        'Prompt user for a number and reflect back thier input
-        'Prompt user for a second number and reflect back both thier inputs
-        'Ask user if they would like to multiply or divide thier input
-        'Return computated value
+        '[~]Prompt user for a number and reflect back thier input
+        '[]Prompt user for a second number and reflect back both thier inputs
+        '[]Ask user if they would like to multiply or divide thier input
+        '[]Return computated value
 
 
         Dim userInput As String
         Dim numberOne As Integer
-        Dim numberTwo As Integer
-        Dim operator As String
+        'Dim numberTwo As Integer
+        'Dim operator As String
+
+        'First Number aquisition
+        Console.WriteLine("Please enter a number.")
+        userInput = Console.ReadLine()
+        Console.WriteLine($"You have entered {userInput}.")
+        '*****WILL CRASH HERE IF INVALID CONVERSION********
+        numberOne = CInt(userInput)
+
+        Console.Read()
 
     End Sub
 
