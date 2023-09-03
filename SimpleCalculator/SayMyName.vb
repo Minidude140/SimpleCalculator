e0a8c1e2f6bd12f5730e37b10ed9e17336e31bed
Zac <zac.minidude140@Gmail.com>
2023-09-02 21:53:57 -0600

    Add project files.

---
+++ SimpleCalculator/SayMyName.vb
@@ -0,0 +1,17 @@
+﻿'Zachary Christensen
+'RCET 2265
+'Fall 2023
+'Simple Calculator
+'https://github.com/Minidude140
+
+
+Option Explicit On
+Option Strict On
+
+Module SayMyName
+
+    Sub Main()
+
+    End Sub
+
+End Module
